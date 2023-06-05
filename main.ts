a191ac3a426b1c4149b63e8a167b90584992d11b
Michael Cadiz <mike@cadizm.com>
2023-06-04 17:16:24 -0700

    Map ctrl-d to delete

--- main.ts
+++ main.ts
@@ -1,137 +1,75 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, MarkdownView, Plugin } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+export default class VimCtrlDPlugin extends Plugin {
+  handlerMap: Map<HTMLElement, boolean>;
 
-interface MyPluginSettings {
-	mySetting: string;
+  registerCtrlDHandlers() {
+    const eventHandler = (event: Event) => {
+      handleKeydown(event, this.app);
+    };
+
+    document.querySelectorAll('.cm-content').forEach((element: HTMLElement) => {
+      if (!this.handlerMap.has(element)) {
+        console.log('Registering event handler for', element.getText());
+        this.registerDomEvent(element, 'keydown', eventHandler);
+        this.handlerMap.set(element, true);
+      }
+    });
+  }
+
+  async onload() {
+    console.log('Loading VimCtrlDPlugin ');
+    this.handlerMap = new Map();
+
+    this.app.workspace.onLayoutReady(() => {
+      // Registering interval clears it on unLoad()
+      this.registerInterval(window.setInterval(() => this.registerCtrlDHandlers(), 2 * 1000));
+    });
+  }
+
+  onunload() {
+    console.log('Unloading VimCtrlDPlugin ');
+    this.handlerMap.clear();
+  }
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const handleKeydown = (event: Event, app: App) => {
+  if (!(event instanceof KeyboardEvent)) {
+    return;
+  }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+  if (event.ctrlKey && event.code == 'KeyD') {
+    const view = app.workspace.getActiveViewOfType(MarkdownView);
+    const editor = view?.editor;
 
-	async onload() {
-		await this.loadSettings();
+    if (!editor) {
+      return;
+    }
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    // @ts-expect-error - getConfig not part of public api
+    const vimMode = app.vault.getConfig('vimMode');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+    // @ts-expect-error - found insertMode through experimentation
+    const insertMode = editor?.cm?.cm?.state?.vim?.insertMode;
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+    if (!vimMode || !insertMode) {
+      return;
+    }
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+    const lastLine = editor.lastLine();
+    const lastCh = editor.getLine(lastLine).length;
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+    const curPos = editor.getCursor();
+    const nextPos = {
+      line: curPos.line,
+      ch: curPos.ch + 1,
+    };
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+    if (curPos.line == lastLine && nextPos.ch > lastCh) {
+      return;
+    }
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
+    // delete next char
+    editor.replaceRange('', curPos, nextPos);
+  }
+};
